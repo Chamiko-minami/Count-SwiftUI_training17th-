587795876e7c4a2252111f7c0d99d2b8ae00c808
Minami SAKAI <minami.gokigen123@gmail.com>
2025-07-21 09:31:37 +0900

    Initial commit

---
+++ Count(SwiftUI_training17th)/Count_SwiftUI_training17th_App.swift
@@ -0,0 +1,17 @@
+//
+//  Count_SwiftUI_training17th_App.swift
+//  Count(SwiftUI_training17th)
+//
+//  Created by 酒井みな実 on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Count_SwiftUI_training17th_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
